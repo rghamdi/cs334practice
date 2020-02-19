f83524e9974dd07d94be34ef0ef4e499692bed1d
Nischal <piyanischal@gmail.com>
2020-02-18 20:43:43 -0500

    Add isGreater function

---
+++ src/cs334demo/calculation.py
@@ -0,0 +1,10 @@
+firstNum = int(input("Enter first number"))
+secNum = int(input("Enter second number"))
+
+def isGreater(firstNumber, secondNumber):
+    if firstNumber > secondNumber:
+        print("First number is greater than the second")
+    else:
+        print("Second number is greater than the first")
+isGreater(firstNum, secNum)
+
